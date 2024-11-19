57fc102996f001d6e7be2579b0294db04436da76
Martin <martin.dinahet@proton.me>
2024-11-19 21:02:07 +0100

    added gitsigns and updated lspconfig

--- lua/punpun/plugins/nvim-lspconfig.lua
+++ lua/punpun/plugins/nvim-lspconfig.lua
@@ -26,11 +26,9 @@ return {
 		})
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
 		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
-		local servers = {}
 		require("mason").setup()
-		local ensure_installed = vim.tbl_keys(servers or {})
-		require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
-
+    local servers = {}
+		require("mason-tool-installer").setup({ ensure_installed = {}})
 		require("mason-lspconfig").setup({
 			handlers = {
 				function(server_name)
