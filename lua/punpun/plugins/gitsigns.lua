853ecfaba2c67b16b02ab2a51598a054abf698ac
Martin <martin.dinahet@proton.me>
2025-02-27 19:14:03 +0100

    added gitsigns

--- lua/punpun/plugins/gitsigns.lua
+++ lua/punpun/plugins/gitsigns.lua
@@ -1,14 +1,17 @@
 return {
   "lewis6991/gitsigns.nvim",
-  event = { "BufReadPost", "BufNewFile" },
+  event = {
+    "BufReadPre",
+    "BufNewFile"
+  },
   opts = {
     signs = {
-      add = { text = "┃"},
-      change = { text = "┃"},
-      delete = { text = "┃"},
-      topdelete = { text = "┃"},
-      changedelete = { text = "┃"},
-      untracked = { text = "┃"},
+      add = { text = "▎" },
+      change = { text = "▎" },
+      delete = { text = "" },
+      topdelete = { text = "" },
+      changedelete = { text = "▎" },
+      untracked = { text = "▎" },
     },
   },
 }
