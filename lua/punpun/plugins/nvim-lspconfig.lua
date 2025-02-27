bccec9be1e632aa78647a20e884c2184baf00227
Martin <martin.dinahet@proton.me>
2025-02-27 19:19:34 +0100

    updated lspconfig to feature icons and added luarc

--- lua/punpun/plugins/nvim-lspconfig.lua
+++ lua/punpun/plugins/nvim-lspconfig.lua
@@ -1,42 +1,69 @@
 return {
-	"neovim/nvim-lspconfig",
-	event = { "BufReadPost", "BufNewFile" },
-	dependencies = {
-		{ "williamboman/mason.nvim", config = true },
-		{ "williamboman/mason-lspconfig.nvim" },
-		{ "WhoIsSethDaniel/mason-tool-installer.nvim" },
-		{ "hrsh7th/cmp-nvim-lsp" },
-	},
-	config = function()
-		vim.api.nvim_create_autocmd("LspAttach", {
-			group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
-			callback = function(event)
-				local client = vim.lsp.get_client_by_id(event.data.client_id)
-				if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-					vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
-					vim.api.nvim_create_autocmd("LspDetach", {
-						group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
-						callback = function(event2)
-							vim.lsp.buf.clear_references()
-							vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
-						end,
-					})
-				end
-			end,
-		})
-		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
-		require("mason").setup()
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPost", "BufNewFile" },
+  dependencies = {
+    {
+      "williamboman/mason.nvim",
+      config = true
+    },
+    {
+      "williamboman/mason-lspconfig.nvim"
+    },
+    {
+      "WhoIsSethDaniel/mason-tool-installer.nvim"
+    },
+    {
+      "hrsh7th/cmp-nvim-lsp"
+    },
+  },
+  config = function()
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+      callback = function(event)
+        local client = vim.lsp.get_client_by_id(event.data.client_id)
+        if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+          vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
+          vim.api.nvim_create_autocmd("LspDetach", {
+            group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
+            callback = function(event2)
+              vim.lsp.buf.clear_references()
+              vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
+            end,
+          })
+        end
+      end,
+    })
+    -- Define signs for diagnostics
+    local signs = { Error = "", Warn = "", Hint = "", Info = "" }
+    for type, icon in pairs(signs) do
+      local hl = "DiagnosticSign" .. type
+      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+    end
+
+    -- Configure diagnostic display
+    vim.diagnostic.config({
+      virtual_text = {
+        prefix = '●',
+      },
+      signs = true,
+      underline = true,
+      update_in_insert = true,
+      severity_sort = true,
+    })
+
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+    require("mason").setup()
     local servers = {}
-		require("mason-tool-installer").setup({ ensure_installed = {}})
-		require("mason-lspconfig").setup({
-			handlers = {
-				function(server_name)
-					local server = servers[server_name] or {}
-					server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-					require("lspconfig")[server_name].setup(server)
-				end,
-			},
-		})
-	end,
+    require("mason-tool-installer").setup({ ensure_installed = {} })
+    require("mason-lspconfig").setup({
+      handlers = {
+        function(server_name)
+          local server = servers[server_name] or {}
+          server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+          require("lspconfig")[server_name].setup(server)
+        end,
+      },
+    })
+  end,
 }
