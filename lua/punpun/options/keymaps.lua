10fdbcbe4d425eb6cc7564fb8ecec7537e0eef04
Martin <martin.dinahet@proton.me>
2025-03-17 15:38:34 +0100

    updated keymaps

--- lua/punpun/options/keymaps.lua
+++ lua/punpun/options/keymaps.lua
@@ -1,15 +1,19 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-local function map(shortcut, command)
+local function map(shortcut, command, mode)
+  if mode == nil then mode = "n" end
   local opts = { noremap = true, silent = true }
-  vim.keymap.set("n", shortcut, "<cmd>" .. command .. "<cr>", opts)
+  vim.keymap.set(mode, shortcut, "<cmd>" .. command .. "<cr>", opts)
 end
 
 
 map("<esc>", "nohlsearch")
+map("<a-d>", "Lspsaga term_toggle", "n")
+map("<a-d>", "Lspsaga term_toggle", "t")
 map("<leader>f", "Telescope find_files")
 map("<leader>ld", "Lspsaga peek_definition")
 map("<leader>lt", "Lspsaga peek_type_definition")
 map("<leader>lr", "Lspsaga rename")
 map("<leader>lf", "Lspsaga finder")
+
